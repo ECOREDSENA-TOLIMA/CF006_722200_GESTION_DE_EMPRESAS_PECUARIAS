8e649b77ddf4f170d19cfb623e95e17243471f18
DaveRnC <davison.gaitan@gmail.com>
2023-09-19 21:08:18 -0500

    terma 4

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -7,13 +7,202 @@
         span 4
       h1 Insumos
       
+    .row.mb-5.align-content-center
+      .col-md-4(data-aos="fade-right")
+        img(src='@/assets/curso/temas/tema4/1.svg' alt='')
+      .col-md-8(data-aos="fade-right")
+        p.mb-3 Todos los procesos productivos pecuarios requieren de cierta cantidad de insumos para poder desarrollarse plenamente y alcanzar los niveles de productividad y rentabilidad esperados, pues es muy difícil que en la misma unidad productiva se encuentren todos los elementos necesarios para el sostenimiento de los animales y la atención de sus necesidades básicas.
+        .tarjeta.p-3(style="border-radius: 0; background-color:#FFFAD0")
+          p En vista de lo anterior, se hace necesario la consecución de insumos de diferente tipo, los cuales tienen diversas funciones al interior de la explotación pecuaria. Entre los principales grupos de insumos que se pueden encontrar en estos sistemas productivos, de acuerdo con su uso, están los siguientes:
+
+    AcordionA.mb-5(tipo="a" clase-tarjeta="tarjeta" data-aos="fade-down-left")(style="background-color: #DBF9BD")
+      .row(titulo="Protección y manejo animal").justify-content-center
+        .col-2
+          figure
+            img(src='@/assets/curso/temas/tema4/2.svg', alt='')
+        .col-10
+          p hace referencia a los medicamentos veterinarios los cuales son sustancias que pueden ser aplicadas o administradas a los animales, con fines terapéuticos, profilácticos, inmunológicos, de diagnóstico o para modificar las funciones fisiológicas y de comportamiento (Oirsa, 2013). Hacen parte de este grupo los antiparasitarios, antibióticos, relajantes musculares, anestésicos y vacunas entre otros.
+        
+      .row(titulo="Adecuación y construcción").justify-content-center
+        .col-2
+          figure
+            img(src='@/assets/curso/temas/tema4/3.svg', alt='')
+        .col-10
+          p Forman parte de este grupo los elementos necesarios para adelantar las labores de construcción y/o adecuación de infraestructura productiva entre los cuales se encuentran cemento, ladrillo, arena, madera, guadua y tejas de zinc entre otros productos. 
+      .row(titulo="Aseo y desinfección").justify-content-center
+        .col-2
+          figure
+            img(src='@/assets/curso/temas/tema4/4.svg', alt='')
+        .col-10
+          p Son los materiales empleados en la limpieza y desinfección de instalaciones, como galpones, bodegas, alojamientos, unidades sanitarias, corrales y camas; equipos y utensilios (comederos, bebederos, fumigadoras) y la desinfección del calzado a la entrada de cada área. Entre los materiales más comunes que se pueden encontrar en este grupo están cal, amonio cuaternario, detergentes, desinfectantes fenólicos e hipoclorito de sodio.
+        
+      .row(titulo="Alimentación").justify-content-center
+        .col-2
+          figure
+            img(src='@/assets/curso/temas/tema4/5.svg', alt='')
+        .col-10
+          p Se refiere a todos los productos empleados para satisfacer las necesidades nutricionales de los animales de la unidad productiva, generalmente denominados “concentrados” y sales. Estos son ricos en principios nutritivos y se emplean como suplemento de ensilados, granos, pastos y demás productos que consumen los animales de forma natural. 
+      .row(titulo="Manejo de praderas").justify-content-center
+        .col-2
+          figure
+            img(src='@/assets/curso/temas/tema4/6.svg', alt='')
+        .col-10
+          p Son todos los insumos que se tengan en la unidad productiva para el sostenimiento de las praderas o pasturas naturales que sirven como fuente primaria de alimento para las especies pecuarias. Dentro de estos insumos es posible encontrar acondicionadores de suelo, fertilizantes, herbicidas y demás productos empleados para este propósito. 
+        
+      .row(titulo="Alimentación").justify-content-center
+        .col-2
+          figure
+            img(src='@/assets/curso/temas/tema4/7.svg', alt='')
+        .col-10
+          p En muchas de las actividades pecuarias es común la presencia de plagas (especialmente roedores e insectos) los cuales además de deteriorar la calidad de los elementos almacenados en la unidad productiva, son vectores con la capacidad de transmitir diferentes enfermedades. En vista de lo anterior, el control de plagas es una de las actividades clave para mantener los principios de bioseguridad. Dentro de los insumos que se encuentran al interior de la unidad productiva para el manejo de plagas están los insecticidas, repelentes, cebos tóxicos y raticidas entre otros.       
+
+    p(data-aos="fade-left").mb-5 El manejo y utilización de estos insumos debe ser supervisado siempre, por lo que es necesario establecer protocolos para realizar estas labores e, igualmente, dejar trazabilidad cada vez que se haga uso de ellos. Por otro lado, es importante almacenar estos elementos de forma adecuada, debidamente rotulados y en las condiciones establecidas por el fabricante en la etiqueta y en la ficha técnica. La bodega donde se almacenen estos elementos debe estar bajo llave, debidamente ventilada y construida con materiales que faciliten su aseo y desinfección y además aseguren la estabilidad de los ingredientes activos.
+
     separador
     #t_4_1.titulo-segundo.color-acento-contenido
       h2 4.1 Ficha técnica
 
+    .row.justify-content-center.mt-5.mb-5 
+      .col
+        .row.justify-content-center
+          .col.lg-2
+            figure(data-aos="fade-right")
+              img(src='@/assets/curso/temas/tema4/8.svg', alt='Texto que describa la imagen')
+          .col-10
+            p(data-aos="fade-left") Todos los productos que se empleen en la industria pecuaria deben estar debidamente registrados por la autoridad correspondiente. En el caso de los medicamentos, alimentos y demás insumos empleados para el manejo de los animales, estos productos deben tener, obligatoriamente, el registro ICA.
+            p(data-aos="fade-left") Para garantizar la seguridad del personal encargado de la manipulación de los insumos y que se les dé un correcto uso, cada uno de ellos tiene lo que se conoce como la ficha técnica que consiste en un documento que da información importante al usuario sobre las características y propiedades del producto, así como de los posibles riesgos y las recomendaciones que se deben tener en cuenta para su empleo de forma correcta.
+
+    .text-decor.text-white.text-center(data-aos="flip-up").mb-5
+      p.mt-3.p-2 Contenido de la ficha técnica
+
+    p(data-aos="fade-left").mb-5 Toda ficha técnica, de cualquier producto, debe contener al menos la siguiente información:
+
+    AcordionA(tipo="b" clase-tarjeta="tarjeta")(style="background-color: #DBF9BD")
+      .row(titulo="Nombre comercial del producto")
+        .col
+          p Se debe indicar claramente el nombre comercial del producto, tal como aparece en el registro ICA. 
+
+      .row(titulo="Ingredientes activos")
+        .col
+          p Mencionar los ingredientes activos que componen el producto.
+
+      .row(titulo="Composición")
+        .col
+          p Expresar de manera clara de qué está hecho el producto y los contenidos exactos de cada uno de los ingredientes activos, especificando con detalle, el número de unidades por determinada cantidad de producto comercial.
+
+      .row(titulo="Mecanismo de acción")
+        .col
+          p Describir detalladamente la forma como el producto realiza el proceso para el cual es utilizado. En el caso de los desinfectantes, por ejemplo, en la ficha técnica se especificará si la acción se efectúa lesionando los ácidos nucleicos, lesionando la membrana celular o inactivando las proteínas de los microorganismos.
+      .row(titulo="Características")
+        .col
+          p Se describen las propiedades más relevantes del producto de manera que orienten al usuario final sobre lo que puede esperar con su utilización. 
+
+      .row(titulo="Instrucciones para uso y dosificación")
+        .col
+          p Se presentan de manera las pautas para la preparación del producto, el tipo de aplicación y las dosis recomendadas de acuerdo con el uso. Es muy importante especificar claramente las unidades en las que se manejarán las dosis para evitar efectos adversos o pérdidas de efectividad de la aplicación. 
+      .row(titulo="Precauciones")
+        .col
+          p Es importante que en la ficha se den recomendaciones frente a las precauciones que se deben tener al momento de utilizar el producto, los usos no permitidos y los elementos de protección que se deben emplear para su utilización de manera segura.  
+
+      .row(titulo="Presentación")
+        .col
+          p En este punto se especifican las presentaciones comerciales en las cuales es posible conseguir el producto.  
+      .row(titulo="Registro sanitario")
+        .col
+          p Es sumamente importante que en la ficha técnica del producto se indique el número de registro sanitario expedido por el ICA, pues en las explotaciones pecuarias no es permitido el uso de productos comerciales sin el visto bueno del Instituto Colombiano Agropecuario. 
+
+      .row(titulo="Grado de toxicidad")
+        .col
+          p En la actualidad existen 4 categorías toxicológicas en los productos agroquímicos: Categoría IA – IB Extremadamente tóxico, color rojo. Categoría II altamente tóxicos, color amarillo. Categoría III medianamente tóxicos, color azul. Categoría IV ligeramente tóxicos color verde.
+      .row(titulo="Posible antídoto")
+        .col
+          p Presenta alternativas al personal médico para el manejo de eventuales intoxicaciones.  
+
+      .row(titulo="Periodo de carencia")
+        .col
+          p Se define como el tiempo que debe transcurrir entre la última aplicación del producto agroquímico y la cosecha del producto. 
+      .row(titulo="Periodo de reingreso")
+        .col
+          p Hace referencia al tiempo que debe transcurrir entre la aplicación del producto y el ingreso de personas o animales al área tratada con el producto.
+
+            
     separador
     #t_4_2.titulo-segundo.color-acento-contenido
       h2 4.2 Protocolo
+
+    p(data-aos="fade-left") Un protocolo se define como un conjunto de medidas, instrucciones o procedimientos que permiten orientar y controlar el desarrollo de una acción específica. Un protocolo se convierte en una guía que indica a un grupo de personas cómo actuar frente a determinada situación, de manera articulada y secuencial, evitando la toma de decisiones a título personal y de manera improvisada.
+    p(data-aos="fade-left") En relación con la bioseguridad, es de suma importancia apegarse a los protocolos establecidos para el manejo de las explotaciones pecuarias, ya que a través de ellos se definen la periodicidad de las labores de limpieza y desinfección, el control de ingreso y el tipo de insumos que se deben emplear para las labores de mantenimiento de la unidad productiva, entre otras acciones.
+    p(data-aos="fade-left").mb-5 Los protocolos más comunes usados en los sistemas pecuarios son los siguientes:
+
+    .titulo-sexto.color-acento-contenido
+      h5 Figura 12
+      span #[em Tipos de protocolo pecuarios]
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-10.col-md-10.col-lg-10.desktop(data-aos="fade-right")
+        figure
+          img(src='@/assets/curso/temas/tema4/9.svg', alt='')
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-10.col-md-10.col-lg-10.movil(data-aos="fade-right")
+        figure
+          img(src='@/assets/curso/temas/tema4/9-m.svg', alt='')
+
+    p(data-aos="fade-left").mb-5 Para la construcción de un protocolo, sin importar su área de acción, se deben tener en cuenta los siguientes elementos:
+
+    SlyderF(columnas="col-lg-6 col-xl-4").mb-5
+      .tarjeta.p-4(style="background-color:#DBF9BD")
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/tema4/10.svg' alt='AvatarTop')
+        h4.text-center Encabezado
+        p.text-center Se define nombre del protocolo, fecha de elaboración y responsables de elaboración, revisión y aprobación.  
+
+      .tarjeta(style="background-color:#DBF9BD").p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/tema4/11.svg' alt='AvatarTop')
+        h4.text-center Objeto
+        p.text-center Se expresa cuál es el fin del protocolo, es decir, qué se pretende con su implementación. 
+
+      .tarjeta(style="background-color:#DBF9BD").p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/tema4/12.svg' alt='AvatarTop')
+        h4.text-center Alcance
+        p.text-center Se delimita la escala de aplicación del protocolo: si aplica en un área de la empresa, en toda la empresa o un área geográfica definida.
+
+
+      .tarjeta(style="background-color:#DBF9BD").p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/tema4/13.svg' alt='AvatarTop')
+        h4.text-center Definiciones
+        p.text-center Se incluye un glosario de términos para centrar al lector sobre el contenido del protocolo. 
+
+      .tarjeta(style="background-color:#DBF9BD").p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/tema4/14.svg' alt='AvatarTop')
+        h4.text-center Condiciones generales
+        p.text-center Se expresan las disposiciones que se deben seguir para garantizar el cumplimiento del objeto del protocolo como lavado de manos, desinfección de superficies, uso de equipos de protección personal, entre otras. 
+
+      .tarjeta(style="background-color:#DBF9BD").p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/tema4/15.svg' alt='AvatarTop')
+        h4.text-center Contenido
+        p.text-center Se definen uno a uno los procedimientos que se deben realizar para el cumplimiento de los objetivos del protocolo, expresando las actividades de manera secuencial y clara.
+
+      .tarjeta(style="background-color:#DBF9BD").p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/tema4/16.svg' alt='AvatarTop')
+        h4.text-center Control de cambios
+        p.text-center Se registran todos los ajustes que han sufrido el protocolo y los procedimientos. Se deja trazabilidad de estos cambios. 
+
+    p(data-aos="fade-left").mb-5 Es posible incluir anexos a los protocolos de limpieza los cuales permitirán al lector obtener información detallada sobre los implementos a emplear, los artículos e insumos. Algunos ejemplos de anexos son las fichas técnicas de los productos de limpieza, el manual de uso de los equipos de protección personal o los manuales de operación de cualquier otro equipo empleado en alguno de los procesos y procedimientos.
+
 </template>
 
 <script>
